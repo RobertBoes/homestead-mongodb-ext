7c189a42269804c321052842cf37e758fc6cb011
Robert Boes <bitbucket@robertboes.nl>
2016-11-16 09:25:57 +0100

    Update readme and bash script

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# homestead-mongodb-ext
-Bash script to install MongoDB extension in a homestead box
+# Homestead MongoDB PHP Extension
+Bash script to install MongoDB PHP extension in a homestead box
+
+## Usage
+
+sudo curl -sS https://raw.githubusercontent.com/RobertBoes/homestead-mongodb-ext/master/homestead-mongodb-ext.sh | sudo sh
---
+++ homestead-mongodb-ext.sh
@@ -0,0 +1,18 @@
+echo "Install script for MongoDB PHP extension in a homestead box"
+
+echo "Updating the packages list";
+sudo apt-get update;
+
+echo "Installing OpenSSl Libraries";
+sudo apt-get install -y autoconf g++ make openssl libssl-dev libcurl4-openssl-dev;
+sudo apt-get install -y libcurl4-openssl-dev pkg-config;
+sudo apt-get install -y libsasl2-dev;
+
+echo "Installing the MongoDB extension";
+sudo pecl install mongodb;
+
+echo "Adding the extension to .ini files";
+sudo echo  "extension = mongodb.so" >> /etc/php/7.0/cli/php.ini;
+sudo echo  "extension = mongodb.so" >> /etc/php/7.0/fpm/php.ini;
+
+echo "Done installing MongoDB extension, have fun!";
